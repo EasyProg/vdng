709d2c5802f6a45990fad25ae29042c75bb4f0f7
Kovka Mykhailo <kovkamy@gmail.com>
2017-10-31 10:16:31 +0200

    sd

---
+++ src/components/ProgramList.js
@@ -0,0 +1,146 @@
+import React, {Component,PropTypes} from 'react';
+import '../styles/css/main_styles.css';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import HomeButton from './ui/MenuButton';
+import * as $ from 'jquery';
+import CustomScroll from './ui/CustomScroll';
+export default class ProgramList extends Component
+                                     {
+    constructor(props)
+                                     {
+        super(props);
+        this.state={itemChosen:0};
+        this.switchProgram = this.switchProgram.bind(this);
+        this.handleKey =     this.handleKey.bind(this);
+                                     }
+
+    static propTypes =               {
+        programs: PropTypes.array.isRequired,
+        visible:  PropTypes.bool.isRequired
+
+                                     };
+    handleKey(e,elem)                {
+    switch (e.key)                   {
+        case 40:
+        this.switchProgram('next', this.state.itemChosen);
+        break;
+        case 38:
+        this.switchProgram('prev', this.state.itemChosen);
+        break;
+                                     }
+                                     }
+    switchProgram(param='next',chosen){
+        var items = $('.programListItem');
+        var i =     $('.programListItemChosen')[0].key||chosen||0;
+        var nextElem = i + 1 >=    items.length ?  0 : i + 1;
+        var prevElem = i - 1 < 0 ? items.length -  1 : i - 1;
+        if (param === 'next'&&items[nextElem])
+                                 {
+            items[nextElem].focus();
+            this.setState({itemChosen:nextElem});
+                                 }
+        if (param === 'prev'&&items[prevElem])
+                                 {
+            items[prevElem].focus();
+            this.setState({itemChosen:prevElem});
+
+                                 }
+                                 }
+    getDayOfWeek (dt)                {
+        var date_parse = new Date(dt.substr(6,4),Number(dt.substr(3,2))-1,dt.substr(0,2));
+        switch (date_parse.getDay()) {
+            case 0 : return 'Неділя';
+                break;
+            case 1 : return 'Понеділок';
+                break;
+            case 2 : return 'Вівторок';
+                break;
+            case 3 : return 'Середа';
+                break;
+            case 4 : return 'Четвер';
+                break;
+            case 5 : return 'П\'ятниця';
+                break;
+            case 6 : return 'Субота';
+                break;
+
+        }
+        //return date_parse.getDay();
+
+                                    }
+    runningString(e)             {
+        var str   = $('.programName_hover:hover');
+        var strCont = $('.programName:hover');
+        var width = str.width();
+        var con_w = str.css('left');
+
+        function run ()           {
+            var con_len = parseInt(con_w) - (width - strCont.width());
+            str.animate
+            ({left:con_len + 'px'},
+                {duration: 3000,
+                    complete: function ()
+                    {
+                        str.css('left',con_w);
+                        //run();
+                    }});          }
+        if (width>strCont.width())
+                                  {
+            run();
+                                  }}
+    stopRun ()                    {
+        $('.programName_hover').stop(true,true);
+                                  }
+    componentDidMount()           {
+        $('.programListItemChosen').focus();
+                                  }
+    componentDidUpdate() {
+        $('.programListItemChosen').focus();
+                                  }
+// $('.programList').animate({'width':'400'},100);
+//                                   }
+    render()                      {
+        if (this.props.programs.length&&this.props.visible>0)
+            return                (
+                <div className="programList">
+                    <div className="menuHeaderCh">
+                        <HomeButton visible={true}/>
+                    </div>
+                    <CustomScroll>
+                        {this.props.programs.map((e,i)=>
+                            <div className="blockChainDiv" key={i}>
+                                <div className="headerProgramDate">
+                                    {e.date} <span className="textSpan">{this.getDayOfWeek(e.date)}</span>
+                                    <hr className="hrProgram"/>
+                                </div>
+                                <div className="dayListItem">
+                                {
+                                        this.props.programs[i]['data'].map
+                                        (
+                                            (elem,i)=>
+                                                <div    key={i}
+                                                        tabIndex={i}
+                                                        className={elem.id===this.props.currentProgramId?"programListItemChosen":"programListItem"}
+                                                        onMouseOver={(e)=>this.runningString(e)}
+                                                        onMouseLeave={(e)=>this.stopRun()}
+                                                     // onKeyDown={this.handleKey(e,elem)}
+                                                >
+                                                        <span className="programTime">{elem.start_time_show.substring(elem.start_time_show.indexOf(':'),
+                                                            elem.start_time_show.length-1).length===1?elem.start_time_show+'0':elem.start_time_show}
+                                                        </span>
+                                                    <span className="programName">
+                                                    <span className="programName_hover">{elem.title}</span>
+                                                    </span>
+                                                </div>
+                                        )
+                                }
+                                </div>
+                            </div>
+                        )}
+                    </CustomScroll>
+                    <div className="menuBottom"/>
+                </div>
+            );
+        else return null
+    }
+}
---
+++ src/components/ui/ChannelUnavailable.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Михаил on 28.10.2017.
+ */
+import React, {Component,PropTypes} from 'react';
+import play from '../../img/play-button-play.svg';
+import '../../styles/css/main_styles.css';
+import * as $ from 'jquery';
+import {bindActionCreators} from 'redux'
+import  {connect} from 'react-redux';
+import {toggleAutoPlay} from '../../actions/actions';
+//default Component
+export default class ChannelUnavailable extends Component
+{
+    constructor(props)
+    {
+        super(props);
+    }
+
+    render()    {
+        return  (
+            <div onMouseMove={this.props.onMouseMove}>
+                <div className="nonPlayingDivError">
+                    {/*<img src={play} className="playHoldImg" onClick={this.props.onClick} onKeyDown={this.props.onKeyDown} tabIndex={1}/>*/}
+                    <div className="bottomTextUn">Канал недоступен</div>
+                </div>
+            </div>
+        )
+    }
+}
