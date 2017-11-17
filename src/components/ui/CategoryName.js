bf849fe6fdccf9b95c21b6edb5c90c60a727c1d0
Kovka Mykhailo <kovkamy@gmail.com>
2017-11-17 10:17:11 +0200

    menu_chabges

---
+++ src/components/ui/CategoryName.js
@@ -0,0 +1,28 @@
+import React, {Component,PropTypes} from 'react';
+import point from '../../img/pointing-to-left.gif';
+import MenuButton from '../ui/MenuButton';
+import point_right from '../../img/arrow-pointing-to-right.gif';
+import '../../styles/css/main_styles.css';
+import  menu from '../../img/main_menu.gif';
+export default class CategoryName extends Component {
+  constructor(props) {
+   super(props);
+                     };
+  static propTypes = {
+  visible:PropTypes.bool.isRequired,
+  reversed:PropTypes.bool.isRequired
+                     };
+    render(){
+    return  (
+            <div className={this.props.visible?'divCateg':'displayNone'}>
+            <div className="menuHeaderCircleDiv" onClick={(e)=>this.props.categVisibleContext()}>
+            <img src={this.props.reversed?point_right:point} width={20} height={20}/>
+            </div>
+            <div>{this.props.categ}</div>
+            <MenuButton  visible  = {this.props.visible}/>
+            </div>
+            )
+            }
+
+
+                                                    }
