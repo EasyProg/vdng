64c5ef9ca738deb467ec4a43320547733b635533
Mykhailo Kovka <m.kovka@hls.tv>
2017-11-30 20:40:22 +0200

    'zooom'

--- src/components/ui/DetailView.js
+++ src/components/ui/DetailView.js
@@ -9,21 +9,27 @@ super(props);
 render () {
     if (this.props.visible)
     return (
-       <div className="detailEpgView">
+           <div className="detailEpgView">
            <div className="divForEpgImages">
-           {this.props.detail.images[0].main?<img className="mainEpgImage" src={this.props.detail.images[0].main.hd}/>:null}
+           {this.props.detail.images[0]?<img className="mainEpgImage" src={this.props.detail.images[0].main.hd}/>:null}
            <div className="divForSmallEpgImages">
            <div className="watchNow">WATCH NOW</div>
-                   {
-                       <img width={50} height={50} src={this.props.detail.images[0].thumb.hd}/>
-                   }
-               </div>
+            {
+            <img width={50} height={50} src={this.props.detail.images[0].thumb.hd}/>
+            }
+            </div>
            </div>
            <div className="textEpgDiv">
-               <div className="headerTextDiv">{this.props.title}</div>
-               <div className="divForEpgText">{this.props.detail.desc}</div>
+           <div className="headerTextDiv">{this.props.title}</div>
+           <div className="buttonsEpgDiv">
+               {
+                   this.props.detail.tags.map((item,i)=><div className="buttonEpgDiv">{item.name}</div>)
+               }
+
            </div>
-       </div>
+           <div className="divForEpgText">{this.props.detail.desc}</div>
+           </div>
+           </div>
            );
     else return null
           }
